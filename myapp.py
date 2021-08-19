aadfc755165133930d7083a9b6ba62dc5fa0de77
KevinJCbed <62120157+KevinJCbed@users.noreply.github.com>
2021-08-19 17:20:25 -0400

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -10,33 +10,33 @@ df = pd.read_csv(
     "https://raw.githubusercontent.com/KevinJCbed/gdplookup/main/data.csv")
 
 st.dataframe(df.head())
-# df['Year'] = df.REF_DATE.astype(str).str[:4]
+df['Year'] = df.REF_DATE.astype(str).str[:4]
 
-# prices_selection = st.sidebar.selectbox(
-#     "What prices?",
-#     df['Prices'].unique().tolist()
-# )
+prices_selection = st.sidebar.selectbox(
+    "What prices?",
+    df['Prices'].unique().tolist()
+)
 
-# sa_selection = st.sidebar.selectbox(
-#     "What seasonal adjustment?",
-#     df['Seasonal adjustment'].unique().tolist()
-# )
+sa_selection = st.sidebar.selectbox(
+    "What seasonal adjustment?",
+    df['Seasonal adjustment'].unique().tolist()
+)
 
 
-# estimate_selection = st.sidebar.selectbox(
-#     "What GDP component?",
-#     df.Estimates.unique().tolist()
-# )
+estimate_selection = st.sidebar.selectbox(
+    "What GDP component?",
+    df.Estimates.unique().tolist()
+)
 
-# df = df.groupby(['Year', 'Estimates', 'Prices',
-#                 'Seasonal adjustment']).VALUE.sum().reset_index()
+df = df.groupby(['Year', 'Estimates', 'Prices',
+                'Seasonal adjustment']).VALUE.sum().reset_index()
 
-# st.title(f"{estimate_selection},{prices_selection},{sa_selection}")
-# st.dataframe(df.query(
-#     "`Prices` == @prices_selection & `Seasonal adjustment` == @sa_selection & `Estimates` == @estimate_selection")[['Year', 'VALUE']])
-# st.caption("""
-#            Data: Statistics Canada,
-#            *2021 data not complete
-#            """)
+st.title(f"{estimate_selection},{prices_selection},{sa_selection}")
+st.dataframe(df.query(
+    "`Prices` == @prices_selection & `Seasonal adjustment` == @sa_selection & `Estimates` == @estimate_selection")[['Year', 'VALUE']])
+st.caption("""
+           Data: Statistics Canada,
+           *2021 data not complete
+           """)
 
 # st.balloons()
