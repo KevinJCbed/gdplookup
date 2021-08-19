768fb7aa4b32067d4aba5787ed89effe487729d7
KevinJCbed <62120157+KevinJCbed@users.noreply.github.com>
2021-08-19 17:17:48 -0400

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from stats_can import StatsCan
+import os
+
+#sc = StatsCan("https://github.com/KevinJCbed/gdplookup/blob/main/stats_can.h5")
+
+# st.image("https://github.com/KevinJCbed/gdplookup/blob/main/logo.png",
+#          use_column_width=True)
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/KevinJCbed/gdplookup/main/data.csv")
+
+
+df['Year'] = df.REF_DATE.astype(str).str[:4]
+
+prices_selection = st.sidebar.selectbox(
+    "What prices?",
+    df['Prices'].unique().tolist()
+)
+
+sa_selection = st.sidebar.selectbox(
+    "What seasonal adjustment?",
+    df['Seasonal adjustment'].unique().tolist()
+)
+
+
+estimate_selection = st.sidebar.selectbox(
+    "What GDP component?",
+    df.Estimates.unique().tolist()
+)
+
+df = df.groupby(['Year', 'Estimates', 'Prices',
+                'Seasonal adjustment']).VALUE.sum().reset_index()
+
+st.title(f"{estimate_selection},{prices_selection},{sa_selection}")
+st.dataframe(df.query(
+    "`Prices` == @prices_selection & `Seasonal adjustment` == @sa_selection & `Estimates` == @estimate_selection")[['Year', 'VALUE']])
+st.caption("""
+           Data: Statistics Canada,
+           *2021 data not complete
+           """)
+
+st.balloons()
